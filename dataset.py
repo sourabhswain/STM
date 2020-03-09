9e401a2eb21a1d04e9fdb6b082af560bbcf60005
Sourabh Swain <sourabhswain@gmail.com>
2020-03-10 00:02:50 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -2,7 +2,7 @@ import os
 import os.path as osp
 import numpy as np
 from PIL import Image
-
+import json
 import torch
 import torchvision
 from torch.utils import data
@@ -12,28 +12,39 @@ import glob
 class DAVIS_MO_Test(data.Dataset):
     # for multi object, do shuffling
 
-    def __init__(self, root, imset='2017/train.txt', resolution='480p', single_object=False):
+    def __init__(self, root, imset='2017/train.txt', resolution='', single_object=False):
         self.root = root
         self.mask_dir = os.path.join(root, 'Annotations', resolution)
-        self.mask480_dir = os.path.join(root, 'Annotations', '480p')
+        self.mask480_dir = os.path.join(root, 'Annotations', resolution)
         self.image_dir = os.path.join(root, 'JPEGImages', resolution)
         _imset_dir = os.path.join(root, 'ImageSets')
         _imset_f = os.path.join(_imset_dir, imset)
+        _meta_file = os.path.join(root, 'meta.json')
+        
+        with open(_meta_file) as json_file:
+            _meta_data = json.load(json_file)
 
         self.videos = []
         self.num_frames = {}
         self.num_objects = {}
         self.shape = {}
+        self.image_list = {}
         self.size_480p = {}
+        mx = 0
         with open(os.path.join(_imset_f), "r") as lines:
             for line in lines:
                 _video = line.rstrip('\n')
                 self.videos.append(_video)
                 self.num_frames[_video] = len(glob.glob(os.path.join(self.image_dir, _video, '*.jpg')))
-                _mask = np.array(Image.open(os.path.join(self.mask_dir, _video, '00000.png')).convert("P"))
-                self.num_objects[_video] = np.max(_mask)
+                img_frames = sorted(os.listdir(os.path.join(self.image_dir, _video)))
+                self.image_list[_video] = img_frames
+                ff = os.listdir(os.path.join(self.mask_dir, _video))
+                _mask = np.array(Image.open(os.path.join(self.mask_dir, _video, ff[0])).convert("P"))
+                num_objs = len(_meta_data["videos"][_video]["objects"])
+                print("Num objs ", num_objs)
+                self.num_objects[_video] = num_objs
                 self.shape[_video] = np.shape(_mask)
-                _mask480 = np.array(Image.open(os.path.join(self.mask480_dir, _video, '00000.png')).convert("P"))
+                _mask480 = np.array(Image.open(os.path.join(self.mask480_dir, _video, ff[0])).convert("P"))
                 self.size_480p[_video] = np.shape(_mask480)
 
         self.K = 11
@@ -44,9 +55,11 @@ class DAVIS_MO_Test(data.Dataset):
 
 
     def To_onehot(self, mask):
+        #offset = 49
         M = np.zeros((self.K, mask.shape[0], mask.shape[1]), dtype=np.uint8)
         for k in range(self.K):
             M[k] = (mask == k).astype(np.uint8)
+
         return M
     
     def All_to_onehot(self, masks):
@@ -61,20 +74,33 @@ class DAVIS_MO_Test(data.Dataset):
         info['name'] = video
         info['num_frames'] = self.num_frames[video]
         info['size_480p'] = self.size_480p[video]
+        info['obj_ids'] = {}
 
         N_frames = np.empty((self.num_frames[video],)+self.shape[video]+(3,), dtype=np.float32)
         N_masks = np.empty((self.num_frames[video],)+self.shape[video], dtype=np.uint8)
-        for f in range(self.num_frames[video]):
-            img_file = os.path.join(self.image_dir, video, '{:05d}.jpg'.format(f))
+        #print("Check ", self.num_frames[video], len(self.image_list[video]))
+        for f, ff  in enumerate(self.image_list[video]):
+            seq_name = video
+            img_file = os.path.join(self.image_dir, seq_name, ff)
             N_frames[f] = np.array(Image.open(img_file).convert('RGB'))/255.
             try:
-                mask_file = os.path.join(self.mask_dir, video, '{:05d}.png'.format(f))  
-                N_masks[f] = np.array(Image.open(mask_file).convert('P'), dtype=np.uint8)
+                mf = ff
+                mf = mf[-9:][:-4] # -9 for YTVOS
+                mask_file = os.path.join(self.mask_dir, video, ff[-9:][:-4] + '.png')  
+                print("Mask file ", mask_file)
+                if os.path.exists(mask_file):
+                    N_masks[f] = np.array(Image.open(mask_file).convert('P'), dtype=np.uint8)
+                    info['obj_ids'][f] = np.unique(N_masks[f])
+                else:
+                    N_masks[f] = 0
+                    info['obj_ids'][f] = np.array([0])
+
             except:
                 # print('a')
-                N_masks[f] = 255
+                N_masks[f] = 0
         
         Fs = torch.from_numpy(np.transpose(N_frames.copy(), (3, 0, 1, 2)).copy()).float()
+        #print("FS ", Fs.shape)
         if self.single_object:
             N_masks = (N_masks > 0.5).astype(np.uint8) * (N_masks < 255).astype(np.uint8)
             Ms = torch.from_numpy(self.All_to_onehot(N_masks).copy()).float()
@@ -82,6 +108,7 @@ class DAVIS_MO_Test(data.Dataset):
             return Fs, Ms, num_objects, info
         else:
             Ms = torch.from_numpy(self.All_to_onehot(N_masks).copy()).float()
+            print("mask size ", Ms.shape)
             num_objects = torch.LongTensor([int(self.num_objects[video])])
             return Fs, Ms, num_objects, info
 
@@ -89,3 +116,4 @@ class DAVIS_MO_Test(data.Dataset):
 
 if __name__ == '__main__':
     pass
+
